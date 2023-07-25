a7485dc326283825609d6c04ee7f44aa76ae125a
Palamarchuk Ivan <vanavana978@gmail.com>
2023-07-25 15:45:06 +0300

    rewritten trigger function for duration analytics

--- src/main/resources/db/migration/V3__adding_trigger.sql
+++ src/main/resources/db/migration/V3__adding_trigger.sql
@@ -1,4 +1,4 @@
---Функция пересчета последнего time (по какой-то причине, иногда, последовательность инкрементируется 2, а не 1 раза)
+-- Функция пересчета последнего time (по какой-то причине, иногда, последовательность инкрементируется 2, а не 1 раза)
 CREATE OR REPLACE FUNCTION get_prev_time(x_time TIMESTAMP WITHOUT TIME ZONE) RETURNS TIMESTAMP WITHOUT TIME ZONE
     LANGUAGE 'plpgsql'
 AS $BODY$
@@ -8,36 +8,48 @@ BEGIN
 END;
 $BODY$;
 
+
+--Функция поиска последней повторяющейся записи
+CREATE OR REPLACE FUNCTION get_prev_time()
+    RETURNS TIMESTAMP WITHOUT TIME ZONE AS $$
+BEGIN
+    RETURN (
+        SELECT time
+        FROM (
+                 SELECT *,
+                        LAG(hash) OVER(ORDER BY time) as prev_hash
+                 FROM raw_data
+             ) sub
+        WHERE hash != prev_hash
+        ORDER BY time DESC
+        LIMIT 1
+    );
+END;
+$$ LANGUAGE 'plpgsql';
+
 --Триггерная функция пересчета прошедшего времени на ресурсе
 CREATE OR REPLACE FUNCTION update_expected_duration() RETURNS TRIGGER AS $$
 DECLARE
-    prev_duration raw_data.expected_duration%TYPE;
     prev_hash raw_data.hash%TYPE;
-    prev_prev_hash raw_data.hash%TYPE;
-    record_count INTEGER;
 
     prev_time raw_data.time%TYPE;
-    prev_prev_time prev_time%TYPE;
+    prev_prev_time raw_data.time%TYPE;
+    prev_relational_time raw_data.time%TYPE;
+
+    record_count INT;
 BEGIN
+    prev_relational_time := (select get_prev_time());
     prev_time := (select get_prev_time(new.time));
-    prev_prev_time := (select get_prev_time(prev_time));
+    SELECT COUNT(*) INTO record_count FROM raw_data;
 
     select hash into prev_hash from raw_data where raw_data.time = prev_time;
-    select hash into prev_prev_hash from raw_data where raw_data.time = prev_prev_time;
-
-    SELECT COUNT(*) INTO record_count FROM raw_data;
-    IF record_count < 3 OR (new.hash = prev_hash AND prev_hash != prev_prev_hash) THEN
-        update raw_data set expected_duration = CURRENT_TIMESTAMP - prev_time where raw_data.time = prev_time;
-
-    elsif prev_hash = new.hash then
-        select expected_duration into prev_duration from raw_data where raw_data.time = prev_prev_time;
-        update raw_data set expected_duration = prev_duration + (CURRENT_TIMESTAMP - prev_time) where raw_data.time = prev_time;
-
-    elsif prev_hash = prev_prev_hash then
-        select expected_duration into prev_duration from raw_data where raw_data.time = prev_prev_time;
-        update raw_data set expected_duration = prev_duration + (CURRENT_TIMESTAMP - prev_time) where raw_data.time = prev_time;
-    else
+    if(record_count < 3) then
         UPDATE raw_data SET expected_duration = CURRENT_TIMESTAMP - prev_time where raw_data.time = prev_time;
+    elsif (new.hash = prev_hash) then
+        UPDATE raw_data SET expected_duration = CURRENT_TIMESTAMP - prev_relational_time where raw_data.time = prev_time;
+    else
+        prev_prev_time := (select get_prev_time(prev_time));
+        UPDATE raw_data SET expected_duration = CURRENT_TIMESTAMP - prev_prev_time where raw_data.time = prev_time;
     end if;
     RETURN NEW;
 END; $$
