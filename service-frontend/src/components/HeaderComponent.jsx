8a39064396398acd94aee4910b8689b2d323988d
Palamarchuk Ivan <vanavana978@gmail.com>
2023-11-26 16:03:25 +0200

    fixed wrong redirection after registration issue

--- service-frontend/src/components/HeaderComponent.jsx
+++ service-frontend/src/components/HeaderComponent.jsx
@@ -18,7 +18,7 @@ class HeaderComponent extends Component {
                 <header>
                     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                          <a className="text-white d-block h3 text-primary font-weight-bold ml-5" style={{ textDecoration: 'none' }} href="/">Link-shortening service</a>
-                         <a className="text-white d-block h3 text-primary font-weight-bold ml-5" style={{ textDecoration: 'none' }} href="/main">Profile</a>
+                             <a className="text-white d-block h3 text-primary font-weight-bold ml-5" style={{ textDecoration: 'none' }} href="/main">Profile</a>
                          <a className="text-white d-block h3 text-primary font-weight-bold mr-5 ml-auto" style={{ textDecoration: 'none' }} href={this.state.LOGOUT_LINK} >Logout</a>
                     </nav>
                 </header>
