81415971ef7f3b5ed5fdd470d5c4845bd9235517
Ivan <vanavana978@gmail.com>
2025-04-21 09:00:56 +0300

    removed target

--- target/classes/db/migration/V2__creating-hyper-table.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS timescaledb;
-
-SELECT create_hypertable(
-               'data', 'time',
-                chunk_time_interval => INTERVAL '1 day',
-                migrate_data => true
-           );
-
-select add_retention_policy('data', INTERVAL '14 day');
